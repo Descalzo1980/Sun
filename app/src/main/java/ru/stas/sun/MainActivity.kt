81ebf9ab2324894397812718f4308be13f10af70
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2023-01-19 14:29:08 +0300

    add luna

--- app/src/main/java/ru/stas/sun/MainActivity.kt
+++ app/src/main/java/ru/stas/sun/MainActivity.kt
@@ -3,10 +3,13 @@ package ru.stas.sun
 import android.animation.AnimatorSet
 import android.animation.ArgbEvaluator
 import android.animation.ObjectAnimator
+import android.graphics.drawable.Drawable
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.AccelerateInterpolator
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import ru.stas.sun.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -37,23 +40,27 @@ class MainActivity : AppCompatActivity() {
         val sunYEnd = binding.sky.height.toFloat()
         val heightAnimator = ObjectAnimator
             .ofFloat(binding.sun, "y", sunYStart, sunYEnd)
-            .setDuration(3000)
+            .setDuration(2000)
         heightAnimator.interpolator = AccelerateInterpolator()
         val sunsetSkyAnimator = ObjectAnimator
             .ofInt(binding.sky, "backgroundColor", blueSkyColor, sunsetSkyColor)
-            .setDuration(3000)
+            .setDuration(2000)
         sunsetSkyAnimator.setEvaluator(ArgbEvaluator())
         val nightSkyAnimator = ObjectAnimator
             .ofInt(binding.sky, "backgroundColor", sunsetSkyColor, nightSkyColor)
             .setDuration(1500)
         nightSkyAnimator.setEvaluator(ArgbEvaluator())
 
+        val animator = ObjectAnimator.ofInt(binding.lune, "alpha", 0, 255)
+        animator.duration = 2000
+        animator.start()
+        binding.lune.isVisible = true
+
         val animatorSet = AnimatorSet()
         animatorSet.play(heightAnimator)
             .with(sunsetSkyAnimator)
             .before(nightSkyAnimator)
+            .after(animator)
         animatorSet.start()
-
     }
-
 }
